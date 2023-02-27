58cb7b47a5f06f92505ea9f80483b0dc74b10858
Endorf <dmytro.kosolapenko@gmail.com>
2023-02-27 15:37:24 +0200

    add application

---
+++ app/src/main/java/com/mvp/sharednotes/SharedNotesApplication.kt
@@ -0,0 +1,19 @@
+package com.mvp.sharednotes
+
+import android.app.Application
+import com.mvp.sharednotes.di.AppComponent
+import com.mvp.sharednotes.di.DaggerAppComponent
+
+class SharedNotesApplication : Application() {
+
+    lateinit var appComponent: AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+
+        appComponent = DaggerAppComponent.builder()
+            .context(this)
+            .build()
+
+    }
+}
