074e3530a062c3f9b2beff434faa9cc366df5f7e
Endorf <dmytro.kosolapenko@gmail.com>
2023-02-28 17:03:38 +0200

    create db entities

---
+++ app/src/main/java/com/mvp/sharednotes/data/storage/db/entity/UserEntity.kt
@@ -0,0 +1,15 @@
+package com.mvp.sharednotes.data.storage.db.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user")
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "row_id") val id: Int,
+    @ColumnInfo(name = "username") val userName: String?,
+    @ColumnInfo(name = "name") val name: String?,
+    @ColumnInfo(name = "email") val email: String?,
+    @Embedded val address: AddressEmbeddedEntity? = null
+)
