3dc53b2c3ebea37f6f4741d942f0bae71b4303a0
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-02 11:01:44 +0200

    update storage module

--- app/src/main/java/com/mvp/sharednotes/di/module/StorageModule.kt
+++ app/src/main/java/com/mvp/sharednotes/di/module/StorageModule.kt
@@ -2,16 +2,19 @@ package com.mvp.sharednotes.di.module
 
 import android.content.Context
 import androidx.room.Room
-import com.mvp.sharednotes.data.storage.db.SharedNotesDatabase
+import com.mvp.sharednotes.data.repository.storage.db.SharedNotesDatabase
+import com.mvp.sharednotes.data.repository.storage.preferences.UserInfoDataStore
+import com.mvp.sharednotes.data.repository.storage.preferences.UserInfoDataStoreImpl
 import com.mvp.sharednotes.di.scope.AppScope
 import dagger.Module
 import dagger.Provides
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @Module
 class StorageModule {
 
-    @AppScope
     @Provides
+    @AppScope
     fun provideDatabase(context: Context): SharedNotesDatabase {
         return Room.databaseBuilder(
             context,
@@ -19,4 +22,10 @@ class StorageModule {
             SharedNotesDatabase.DB_NAME
         ).build()
     }
+
+    @Provides
+    @AppScope
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun provideUserInfoDataStore(context: Context): UserInfoDataStore =
+        UserInfoDataStoreImpl(context)
 }
