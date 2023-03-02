e674e7c3e01c06140f7ab1f14516e01bbdf07121
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-02 10:56:15 +0200

    remove outdated classes

--- app/src/main/java/com/mvp/sharednotes/SecondFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.mvp.sharednotes
-
-import androidx.fragment.app.Fragment
-
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment(R.layout.fragment_second)
