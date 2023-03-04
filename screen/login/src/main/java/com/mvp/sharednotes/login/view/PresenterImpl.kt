1f1c6271c91ae1a66773a4a7ca6a61377ec1c93c
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-04 14:40:22 +0200

    temporary update presenter

--- screen/login/src/main/java/com/mvp/sharednotes/login/view/PresenterImpl.kt
+++ screen/login/src/main/java/com/mvp/sharednotes/login/view/PresenterImpl.kt
@@ -1,19 +1,15 @@
 package com.mvp.sharednotes.login.view
 
-import com.mvp.sharednotes.login.LoginView
 import com.mvp.sharednotes.login.view.entity.UserCredentials
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import javax.inject.Inject
 
-class PresenterImpl(
-    private val view: LoginView
+class PresenterImpl @Inject constructor(
 ) : Presenter {
 
     private var disposable = CompositeDisposable()
 
     override fun login(credential: UserCredentials) {
-        view.onLoadingStart()
-        view.onSuccessfulLogin()
-        view.onLoadingFinish()
     }
 
     override fun destroy() {
