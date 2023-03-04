6563ce27728ee67cebd10aac4151e04d2c04d642
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-04 18:59:22 +0200

    add Interactor

---
+++ screen/login/src/main/java/com/mvp/sharednotes/login/domain/LoginInteractor.kt
@@ -0,0 +1,16 @@
+package com.mvp.sharednotes.login.domain
+
+import com.mvp.sharednotes.login.view.entity.User
+import com.mvp.sharednotes.login.view.entity.UserCredentials
+import io.reactivex.rxjava3.core.Single
+import javax.inject.Inject
+
+class LoginInteractor @Inject constructor(
+    private val repository: LoginRepository,
+) : Interactor {
+
+    override fun login(credentials: UserCredentials): Single<User> {
+        return repository.get(credentials)
+            .onErrorResumeNext { repository.create(credentials) }
+    }
+}
