b913932ce2b1a11094adf1a3b28af9978060d0ed
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-05 16:56:03 +0200

    provide RemoteDataStore

--- app/src/main/java/com/mvp/sharednotes/di/module/StorageModule.kt
+++ app/src/main/java/com/mvp/sharednotes/di/module/StorageModule.kt
@@ -5,9 +5,12 @@ import androidx.room.Room
 import com.mvp.sharednotes.data.repository.storage.db.SharedNotesDatabase
 import com.mvp.sharednotes.data.repository.storage.db.dao.UserDao
 import com.mvp.sharednotes.data.repository.storage.UserDataStore
+import com.mvp.sharednotes.data.repository.storage.cloud.RemoteUserDataStore
+import com.mvp.sharednotes.data.repository.storage.cloud.api.UserApi
 import com.mvp.sharednotes.data.repository.storage.db.LocalUserDataStore
 import com.mvp.sharednotes.data.repository.storage.preferences.SharedUserDataStore
 import com.mvp.sharednotes.di.qualifier.Local
+import com.mvp.sharednotes.di.qualifier.Remote
 import com.mvp.sharednotes.di.qualifier.Shared
 import com.mvp.sharednotes.di.scope.AppScope
 import dagger.Module
@@ -35,6 +38,11 @@ class StorageModule {
     @AppScope
     @Local
     fun provideLocalDataStore(userDao: UserDao): UserDataStore = LocalUserDataStore(userDao)
+
+    @Provides
+    @AppScope
+    @Remote
+    fun provideRemoteDataStore(api: UserApi): UserDataStore = RemoteUserDataStore(api)
 
     @Provides
     @AppScope
