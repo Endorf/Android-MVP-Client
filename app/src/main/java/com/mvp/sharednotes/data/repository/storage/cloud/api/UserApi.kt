93ff932d24851a7c57f1b5f48ae1f7c54064f69a
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-05 16:55:01 +0200

    update UserApi

--- app/src/main/java/com/mvp/sharednotes/data/repository/storage/cloud/api/UserApi.kt
+++ app/src/main/java/com/mvp/sharednotes/data/repository/storage/cloud/api/UserApi.kt
@@ -1,6 +1,7 @@
 package com.mvp.sharednotes.data.repository.storage.cloud.api
 
 import com.mvp.sharednotes.data.repository.storage.cloud.entity.UserEntity
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.Body
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
@@ -11,12 +12,13 @@ import retrofit2.http.Path
 interface UserApi {
 
     @GET(GET_ENDPOINT)
-    suspend fun get(@Path(ID) id: Int): UserEntity
+    @Headers(CONTENT_TYPE_APPLICATION_JSON)
+    fun get(@Path(ID) id: Int): Single<UserEntity>
 
     @POST(POST_ENDPOINT)
     @FormUrlEncoded
     @Headers(CONTENT_TYPE_APPLICATION_JSON)
-    suspend fun create(@Body user: UserEntity): UserEntity
+    fun create(@Body user: UserEntity): UserEntity
 
     private companion object {
         const val ID = "id"
