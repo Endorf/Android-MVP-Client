6386a27c2857ef3178296c7bd80672fb1571cc90
Endorf <dmytro.kosolapenko@gmail.com>
2023-03-05 16:55:52 +0200

    mock ID in remote data store

---
+++ app/src/main/java/com/mvp/sharednotes/data/repository/storage/cloud/RemoteUserDataStore.kt
@@ -0,0 +1,28 @@
+package com.mvp.sharednotes.data.repository.storage.cloud
+
+import com.mvp.sharednotes.data.entity.User
+import com.mvp.sharednotes.data.repository.storage.UserDataStore
+import com.mvp.sharednotes.data.repository.storage.cloud.api.UserApi
+import com.mvp.sharednotes.data.repository.storage.cloud.entity.toUser
+import com.mvp.sharednotes.data.repository.storage.cloud.entity.toUserEntity
+import io.reactivex.rxjava3.core.Single
+import javax.inject.Inject
+
+class RemoteUserDataStore @Inject constructor(
+    private val api: UserApi
+) : UserDataStore {
+
+    override fun create(user: User): Single<User> = Single.create {
+        api.create(user.toUserEntity())
+    }
+
+    override fun get(user: User): Single<User> = api.get(MOCK_ID).map { it.toUser() }
+
+    override fun get(): Single<User> {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val MOCK_ID = 1
+    }
+}
